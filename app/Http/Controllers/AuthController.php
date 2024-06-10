180319c6d35cbc66d10dd3db05f8e6bfb2bea41d
muneeb876781 <muneebto1256@gmail.com>
2024-06-10 15:56:51 +0500

    login redirect update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,12 +25,12 @@ class AuthController extends Controller
 
         Auth::login($user);
 
-        return redirect()->route('welcome');
+        return redirect()->route('login');
     }
 
     public function welcome()
     {
-        return view('home');
+        return view('welcome');
     }
 
     public function logout(Request $request)
